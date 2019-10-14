5ec77b0917fdc42b8b67cecedfbae944695ecb04
SFMack <sfcamack@icloud.com>
2019-10-13 20:52:04 -0700

    working on making the search form active"

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,14 +1,27 @@
 import React, { useState } from 'react';
 
 export default function SearchForm() {
+	const [term, setTerm] = useState('');
+
+	const clickHandler = e => {
+		e.preventDefault();
+		setTerm(e.target.value);
+		console.log(e);
+	};
+
 	return (
 		<section className='search-form'>
-			<input
-				type='text'
-				className='input'
-				id='addInput'
-				placeholder='Enter your search term'
-			/>
+			<form>
+				<input
+					type='text'
+					className='input'
+					id='addInput'
+					placeholder='Enter your search term'
+				/>
+				<button type='submit' onClick={clickHandler}>
+					search
+				</button>
+			</form>
 		</section>
 	);
 }
